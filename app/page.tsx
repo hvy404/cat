e88ad8f3b438d6f37cd7a763c16f1ce7121959ce
hvy404 <lehuy.a+github@gmail.com>
2024-04-11 20:37:27 -0400

    Visual cues

--- app/page.tsx
+++ app/page.tsx
@@ -46,31 +46,46 @@ export default function Home() {
     setFileResponse(response); // Set inital resume data to trigger rest of actions
   };
 
+  // Generate static schema
   useEffect(() => {
-    setIsStaticResumeLoading(true);
-    fileResponse &&
-      generateLiftedStatic(fileResponse).then((res) => setResponse(res));
-
-    setIsStaticResumeLoading(false);
+    if (fileResponse) {
+      setIsStaticResumeLoading(true);
+      generateLiftedStatic(fileResponse)
+        .then((res) => {
+          setResponse(res);
+          setIsStaticResumeLoading(false);
+        })
+        .catch((err) => {
+          console.error("Error generating static resume:", err);
+          setIsStaticResumeLoading(false);
+        });
+    }
   }, [fileResponse]);
 
-  const generateInferredDataPoints = async () => {
-    setIsInferredResumeLoading(true);
-    const res = await generateLiftedInferred(fileResponse);
-    setInferResponse(res);
-    setIsInferredResumeLoading(false);
-  };
-
+  // Generate inferred schema
   useEffect(() => {
-    response && generateInferredDataPoints();
-  }, [response]);
+    if (fileResponse && response) {
+      // Only generate inferred schema if static schema is generated
+      setIsInferredResumeLoading(true);
+      generateLiftedInferred(fileResponse)
+        .then((res) => {
+          setInferResponse(res);
+          setIsInferredResumeLoading(false);
+        })
+        .catch((err) => {
+          console.error("Error generating inferred resume:", err);
+          setIsInferredResumeLoading(false);
+        });
+    }
+  }, [fileResponse, response]);
 
   return (
     <div className="mx-auto h-screen max-w-7xl p-4 max-h-[80vh] space-y-8">
-      <div className="mt-12 pb-12 font-semibold text-slate-800">01 Alpha. Defining schema.</div>
+      <div className="mt-12 pb-12 font-semibold text-slate-800">
+        01 Alpha. Defining schema.
+      </div>
       <div className="flex flex-row gap-x-8">
         <div className="w-1/2 border border-1 border-gray-400 rounded-md p-4">
-          {/* <button onClick={handleClick}>Ingest</button> */}
           <Dropzone onDrop={handleFileUpload} multiple={false}>
             {({ getRootProps, getInputProps }) => (
               <section>
@@ -97,7 +112,7 @@ export default function Home() {
         </div>
         <div className="w-1/2 border border-1 border-gray-400 rounded-md p-4 h-[75vh] max-h-[80vh] overflow-y-scroll">
           {isInferredResumeLoading && <p>Generating...</p>}
-          {response && (
+          {inferResponse && (
             <MyCoolCodeBlock
               code={inferResponse}
               language="json"
